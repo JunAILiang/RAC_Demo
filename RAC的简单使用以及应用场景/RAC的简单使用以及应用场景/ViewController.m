a14270e1b7042190613be632d26e5ff1e6afd339
JM <gzliujm@163.com>
2018-04-20 01:47:24 +0800

    no message

--- RAC的简单使用以及应用场景/RAC的简单使用以及应用场景/ViewController.m
+++ RAC的简单使用以及应用场景/RAC的简单使用以及应用场景/ViewController.m
@@ -11,27 +11,48 @@
 
 @interface ViewController ()
 
+/** subscriber */
+@property (nonatomic, strong) id <RACSubscriber>subscriber;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //1.创建信号
+    RACSubject *subject = [RACSubject subject];
     
+    //2.订阅信号
+    [subject subscribeNext:^(id  _Nullable x) {
+        NSLog(@"接收数据X: %@",x);
+    }];
+    
+    //3.发送数据
+    [subject sendNext:@"JM"];
+    
+}
+
+- (void)demo {
     //1.创建信号(冷信号)
     RACSignal *signal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
         //3.发送信号
         [subscriber sendNext:@"我在发送信号"];
+        self.subscriber = subscriber;
         
-        return nil;
+        return [RACDisposable disposableWithBlock:^{
+            //只要信号取消订阅就会进入这个block
+            NSLog(@"取消订阅了");
+        }];
     }];
     
     //2.订阅信号(热信号)
-    [signal subscribeNext:^(id  _Nullable x) {
+    RACDisposable *disposable = [signal subscribeNext:^(id  _Nullable x) {
         NSLog(@"订阅信号 %@", x);
     }];
     
-    
+    //因为有订阅者在, 所有手动取消订阅
+    [disposable dispose];
 }
 
 
